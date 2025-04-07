c24fadffb7e6c8fcd4f2b3ac42777d99690d646e
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-04-07 15:09:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+import configparser
+import ast
 
-json_key = ["firebase"]["key"]
+config = configparser.ConfigParser()
+config.read("secrets.ini")
+json_key_str = config.get("firebase", "key")
+json_key = ast.literal_eval(json_key_str)
 cred = credentials.Certificate(json_key)
 firebase_admin.initialize_app(cred)
